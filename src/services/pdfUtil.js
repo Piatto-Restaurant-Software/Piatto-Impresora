d904af0c04f756f0b2cd9e5a3ec82c4f1e658c21
KathGonzalez <katy06salinas@gmail.com>
2025-08-05 22:46:21 -0600

    feat: vuelto en USB

--- src/services/pdfUtil.js
+++ src/services/pdfUtil.js
@@ -760,11 +760,23 @@ async function designFullTicket(
     await printer.write(`${SEPARATOR}\n`);
     await printer.write(`${translations.payments}\n`);
     for (const pago of ticketData.pagos) {
-      await printer.write(
-        `${pago.tipo_pago.nombre}: ${
-          ticketData.simbolo_moneda
-        }${pago.monto.toFixed(2)}${pago.tarjeta ? ` (${pago.tarjeta})` : ""}\n`
-      );
+      if (pago.tipo_pago.id == 1) {
+        await printer.write(
+          `${pago.tipo_pago.nombre}: ${ticketData.simbolo_moneda}${ticketData.pago_efectivo}\n`
+        );
+
+        await printer.write(
+          `${translations.change}: ${ticketData.simbolo_moneda}${ticketData.vuelto}\n`
+        );
+      } else {
+        await printer.write(
+          `${pago.tipo_pago.nombre}: ${
+            ticketData.simbolo_moneda
+          }${pago.monto.toFixed(2)}${
+            pago.tarjeta ? ` (${pago.tarjeta})` : ""
+          }\n`
+        );
+      }
     }
   }
 
