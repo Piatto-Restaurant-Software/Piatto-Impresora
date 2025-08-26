b16d107658539d82abe900521bd604fa2c29186d
KathGonzalez <katy06salinas@gmail.com>
2025-08-25 22:03:33 -0600

    fix: en ccf mostrar antes el cliente

--- src/services/pdfUtil.js
+++ src/services/pdfUtil.js
@@ -684,6 +684,11 @@ async function designFullTicket(
     billingData != null &&
     billingData.tipo_factura === COMPROBANTE_CREDITO_FISCAL
   ) {
+    if (ticketData.cuenta_venta.nombre_cliente_generico) {
+      await printer.write(
+        `${translations.client}: ${ticketData.cuenta_venta.nombre_cliente_generico}\n`
+      );
+    }
     await printer.write(`${translations.date}: ${billingData.fecha_emision}\n`);
     await printer.write(
       `${translations.codigo_generacion}: ${billingData.codigo_generacion}\n`
@@ -694,11 +699,6 @@ async function designFullTicket(
     await printer.write(
       `${translations.sello_recepcion}: ${billingData.sello_recepcion}\n`
     );
-    if (ticketData.cuenta_venta.nombre_cliente_generico) {
-      await printer.write(
-        `${translations.client}: ${ticketData.cuenta_venta.nombre_cliente_generico}\n`
-      );
-    }
   } else {
     if (ticketData.cuenta_venta.nombre_cliente_generico) {
       await printer.write(
